19b621f029f4c091180f32d53b2a34726ad08386
Charan <puvvadacharansai123@gmail.com>
2024-06-28 15:53:26 +0530

    First Commit

---
+++ GreetingServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GreetingServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String greeting = "Hello, " + username + "!";
+
+        request.setAttribute("greeting", greeting);
+        request.getRequestDispatcher("greet.html").forward(request, response);
+    }
+}
